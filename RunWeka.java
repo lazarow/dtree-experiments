5d4eb476f15a031359661be775f24973322fa17a
Arek Nowakowski <nowakowski.arkadiusz@gmail.com>
2021-10-21 17:58:39 +0200

    wip Weka

---
+++ RunWeka.java
@@ -0,0 +1,12 @@
+package dtree.team;
+
+import weka.core.converters.CSVLoader;
+
+public class RunWeka
+{
+    public static void main(String[] args)
+    {
+        CSVLoader loader = new CSVLoader();
+        System.out.println("Hello World!");
+    }
+}
