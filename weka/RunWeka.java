3e1ee0dc6f7a7ddfca5e20ab0e244b0f2276135d
Arkadiusz Nowakowski <a.nowakowski@devesoft.pl>
2021-10-22 13:23:59 +0200

    wip

--- weka/RunWeka.java
+++ weka/RunWeka.java
@@ -1,12 +1,22 @@
 package dtree.team;
 
 import weka.core.converters.CSVLoader;
+import weka.core.Instances;
+import java.io.File;
+import java.io.IOException;
 
 public class RunWeka
 {
-    public static void main(String[] args)
+    public static void main(String[] args) throws IOException
     {
+        String dataset = "breast-cancer-wisconsin";
+        String trainDataFilepath = "./../datasets/" + dataset + "_trte.data"; 
+        String testDataFilepath = "./../datasets/" + dataset + "_clean.data";
+
         CSVLoader loader = new CSVLoader();
+        loader.setSource(new File(trainDataFilepath));
+        Instances trainData = loader.getDataSet();
+
         System.out.println("Hello World!");
     }
 }
