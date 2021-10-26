cab43c5f5205b15e9073075e933c0259e7f0a101
lazarow <nowakowski.arkadiusz@gmail.com>
2021-10-26 16:57:13 +0200

    YAML for weka

--- weka/RunWeka.java
+++ weka/RunWeka.java
@@ -7,6 +7,7 @@ import java.io.File;
 import org.ho.yaml.Yaml;
 
 import weka.classifiers.trees.J48;
+import weka.classifiers.trees.j48.ClassifierTree;
 import weka.filters.Filter;
 import weka.filters.unsupervised.attribute.NumericToNominal;
 import weka.core.SerializationHelper;
@@ -14,10 +15,17 @@ import weka.classifiers.Evaluation;
 import java.util.ArrayList;
 import java.util.Scanner;
 import java.io.FileReader;
+import java.io.FileWriter;
+import java.text.SimpleDateFormat;  
+import java.util.Date;
+import java.util.Enumeration;
 
 /**
  * Na podstawie:
  * http://aragorn.pb.bialystok.pl/~grekowj/iaid/Wyklady/3.Weka%20in%20Java_v2.pdf
+ * 
+ * Algorytm:
+ * https://weka.sourceforge.io/doc.dev/weka/classifiers/trees/J48.html
  */
 public class RunWeka
 {
@@ -52,8 +60,11 @@ public class RunWeka
             testData = Filter.useFilter(testData, filter);
 
             // Training
-            J48 model = new J48(); 
+            CustomJ48 model = new CustomJ48();
+            long start = System.nanoTime();
             model.buildClassifier(trainData);
+            long finish = System.nanoTime();
+            double timeElapsed = (finish - start) / (double) 1000000000;
 
             // Evaluating
             Evaluation evaluation = new Evaluation(trainData);
@@ -63,20 +74,50 @@ public class RunWeka
             SerializationHelper.write("./../out/c4.5/c4.5_" + dataset + "_tree.model", model);
 
             // Saving the results
-            double treeSize = model.getMeasure("measureTreeSize");
-
-
-            Results results = new Results();
-            results.method = "C4.5";
-            Yaml.dump(results, new File("./object.yml"));
+            FileWriter writer = new FileWriter("./../out/c4.5/c4.5_" + dataset + "_results.yml");
+            writer.write("method: C4.5\n");
+            writer.write("database: " + dataset + "\n");
+            writer.write("confusion matrix:\n");
+            double[][] confusionMatrix = evaluation.confusionMatrix();
+            for (int i = 0; i < confusionMatrix.length; ++i) {
+                for (int j = 0; j < confusionMatrix[i].length; ++j) {
+                    writer.write(j == 0 ? "- " : "  ");
+                    writer.write("- " + confusionMatrix[i][j]);
+                    writer.write("\n");
+                }
+            }
+            writer.write("tree:\n");
+            writer.write("  size: " + (int) model.getMeasure("measureTreeSize") + "\n");
+            writer.write("  height: " + model.getMaxDepth(model.getTreeRoot()) + "\n");
+            writer.write("  serialization: c4.5_" + dataset + "_tree.model\n");
+            SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss z");  
+            Date date = new Date();
+            writer.write("time of experiment: " + formatter.format(date) + "\n");
+            writer.write("cpu time: " + timeElapsed + "\n");
+            writer.close();
         }
     }
 }
 
-class Results
+class CustomJ48 extends J48
 {
-    public String method;
-    public String database;
+    public ClassifierTree getTreeRoot()
+    {
+        return m_root;
+    }
 
-    public Results() {}
+    public int getMaxDepth(ClassifierTree node)
+    {
+        if (node.isLeaf()) {
+            return 1;
+        }
+        int maxDepth = -1;
+        for (ClassifierTree child: node.getSons()) {
+            int childMaxDepth = getMaxDepth(child);
+            if (childMaxDepth > maxDepth) {
+                maxDepth = childMaxDepth;
+            }
+        }
+        return maxDepth + 1;
+    }
 }
