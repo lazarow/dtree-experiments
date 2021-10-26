fd7c01b117b0256ab4349d887446f472ea5e53e2
lazarow <nowakowski.arkadiusz@gmail.com>
2021-10-26 13:44:20 +0200

    wip

--- weka/RunWeka.java
+++ weka/RunWeka.java
@@ -11,6 +11,9 @@ import weka.filters.Filter;
 import weka.filters.unsupervised.attribute.NumericToNominal;
 import weka.core.SerializationHelper;
 import weka.classifiers.Evaluation;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.io.FileReader;
 
 /**
  * Na podstawie:
@@ -20,50 +23,60 @@ public class RunWeka
 {
     public static void main(String[] args) throws Exception
     {
-        // Datasets paths
-        String dataset = "breast-cancer-wisconsin";
-        String trainDataFilepath = "./../datasets/" + dataset + "_trte.data"; 
-        String testDataFilepath = "./../datasets/" + dataset + "_clean.data";
+        ArrayList<String> datasets = new ArrayList<>();
+        try (Scanner s = new Scanner(new FileReader("./../datasets.txt"))) {
+            while (s.hasNext()) {
+                datasets.add(s.nextLine());
+            }
+        }
+        for (String dataset: datasets) {
+            
+            // Datasets paths
+            String trainDataFilepath = "./../datasets/" + dataset + "_trte.data"; 
+            String testDataFilepath = "./../datasets/" + dataset + "_clean.data";
 
-        // Loading data
-        CSVLoader loader = new CSVLoader();
-        loader.setNoHeaderRowPresent(true);
-        loader.setSource(new File(trainDataFilepath));
-        Instances trainData = loader.getDataSet();
-        trainData.setClassIndex(trainData.numAttributes() - 1);
-        loader.setSource(new File(testDataFilepath));
-        Instances testData = loader.getDataSet();
-        testData.setClassIndex(testData.numAttributes() - 1);
+            // Loading data
+            CSVLoader loader = new CSVLoader();
+            loader.setNoHeaderRowPresent(true);
+            loader.setSource(new File(trainDataFilepath));
+            Instances trainData = loader.getDataSet();
+            trainData.setClassIndex(trainData.numAttributes() - 1);
+            loader.setSource(new File(testDataFilepath));
+            Instances testData = loader.getDataSet();
+            testData.setClassIndex(testData.numAttributes() - 1);
 
-        // Formatting data
-        NumericToNominal filter = new NumericToNominal();
-        filter.setInputFormat(trainData);
-        trainData = Filter.useFilter(trainData, filter);
-        testData = Filter.useFilter(testData, filter);
+            // Formatting data
+            NumericToNominal filter = new NumericToNominal();
+            filter.setInputFormat(trainData);
+            trainData = Filter.useFilter(trainData, filter);
+            testData = Filter.useFilter(testData, filter);
 
-        // Training
-        J48 model = new J48(); 
-        model.buildClassifier(trainData);
+            // Training
+            J48 model = new J48(); 
+            model.buildClassifier(trainData);
 
-        // Evaluating
-        Evaluation evaluation = new Evaluation(trainData);
-        evaluation.evaluateModel(model, testData);
+            // Evaluating
+            Evaluation evaluation = new Evaluation(trainData);
+            evaluation.evaluateModel(model, testData);
 
-        // Saving results
-        SerializationHelper.write("./j48.model", model);
-        double treeSize = model.getMeasure("measureTreeSize");
+            // Saving the serialized tree
+            SerializationHelper.write("./../out/c4.5/c4.5_" + dataset + "_tree.model", model);
+
+            // Saving the results
+            double treeSize = model.getMeasure("measureTreeSize");
 
 
-        Results results = new Results();
-        results.name = "C4.5";
-        Yaml.dump(results, new File("./object.yml"));
-
+            Results results = new Results();
+            results.method = "C4.5";
+            Yaml.dump(results, new File("./object.yml"));
+        }
     }
 }
 
 class Results
 {
-    public String name;
+    public String method;
+    public String database;
 
     public Results() {}
 }
